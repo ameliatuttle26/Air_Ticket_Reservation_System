6b27549f0a4ce87d533c5933586a53e5f4c44f53
ameliatuttle26 <aat9362@nyu.edu>
2025-12-08 15:48:58 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "YOUR_MYSQL_PASSWORD",
-    "database": "air_reservation",
-}
-SECRET_KEY = "CHANGE_ME_TO_SOMETHING_RANDOM"
